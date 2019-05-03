3f03a13523e596072d855ec9ddd5970baa36af78
lizhefeng <lizhefeng1993@gmail.com>
2019-05-03 16:49:05 -0700

    Add headers to graphql files

--- graphql/server/server.go
+++ graphql/server/server.go
@@ -1,3 +1,12 @@
+// Copyright (c) 2019 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+// usage: go build -o ./bin/server -v ./graphql/server
+// ./bin/server
+
 package main
 
 import (
