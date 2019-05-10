ca50cb89b6dad8a2711216d9ca62ec5052de7a96
lizhefeng <lizhefeng1993@gmail.com>
2019-05-09 18:19:37 -0700

    Add votings protocol

---
+++ protocol/votings/protocol.go
@@ -0,0 +1,242 @@
+package votings
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"math"
+	"strconv"
+
+	"github.com/iotexproject/iotex-core/action/protocol/poll"
+	"github.com/iotexproject/iotex-core/blockchain/block"
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
+	"github.com/iotexproject/iotex-election/pb/api"
+	"github.com/iotexproject/iotex-proto/golang/iotexapi"
+	"github.com/pkg/errors"
+
+	"github.com/iotexproject/iotex-analytics/indexcontext"
+	"github.com/iotexproject/iotex-analytics/protocol"
+	s "github.com/iotexproject/iotex-analytics/sql"
+)
+
+const (
+	// ProtocolID is the ID of protocol
+	ProtocolID = "voting"
+	// VotingHistoryTableName is the table name of voting history
+	VotingHistoryTableName = "voting_history"
+	// VotingResultTableName is the table name of voting result
+	VotingResultTableName = "voting_result"
+)
+
+type (
+	// VotingHistory defines the schema of "voting history" table
+	VotingHistory struct {
+		EpochNumber       uint64
+		CandidateName     string
+		VoterAddress      string
+		Votes             string
+		WeightedVotes     string
+		RemainingDuration string
+	}
+
+	// VotingResult defines the schema of "voting result" table
+	VotingResult struct {
+		EpochNumber        uint64
+		DelegateName       string
+		OperatorAddress    string
+		RewardAddress      string
+		TotalWeightedVotes string
+	}
+)
+
+// Protocol defines the protocol of indexing blocks
+type Protocol struct {
+	Store        s.Store
+	NumDelegates uint64
+	NumSubEpochs uint64
+}
+
+// NewProtocol creates a new protocol
+func NewProtocol(store s.Store, numDelegates uint64, numSubEpochs uint64) *Protocol {
+	return &Protocol{Store: store, NumDelegates: numDelegates, NumSubEpochs: numSubEpochs}
+}
+
+// CreateTables creates tables
+func (p *Protocol) CreateTables(ctx context.Context) error {
+	// create voting history table
+	if _, err := p.Store.GetDB().Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"([epoch_number] INT NOT NULL, [candidate_name] TEXT NOT NULL, [voter_address] TEXT NOT NULL, [votes] TEXT NOT NULL, "+
+		"[weighted_votes] TEXT NOT NULL, [remaining_duration] TEXT NOT NULL)",
+		VotingHistoryTableName)); err != nil {
+		return err
+	}
+
+	// create voting result table
+	if _, err := p.Store.GetDB().Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"([epoch_number] INT NOT NULL, [delegate_name] TEXT NOT NULL, [operator_address] TEXT NOT NULL, "+
+		"[reward_address] TEXT NOT NULL, [total_weighted_votes] TEXT NOT NULL)",
+		VotingResultTableName)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Initialize initializes rewards protocol
+func (p *Protocol) Initialize(ctx context.Context, tx *sql.Tx, genesisCfg *protocol.GenesisConfig) error {
+	return nil
+}
+
+// HandleBlock handles blocks
+func (p *Protocol) HandleBlock(ctx context.Context, tx *sql.Tx, blk *block.Block) error {
+	height := blk.Height()
+	epochNumber := protocol.GetEpochNumber(p.NumDelegates, p.NumSubEpochs, height)
+
+	if height == protocol.GetEpochHeight(epochNumber, p.NumDelegates, p.NumSubEpochs) {
+		indexCtx := indexcontext.MustGetIndexCtx(ctx)
+		chainClient := indexCtx.ChainClient
+		electionClient := indexCtx.ElectionClient
+
+		candidates, gravityHeight, err := p.getCandidates(chainClient, electionClient, height)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get candidates from election service in epoch %d", epochNumber)
+		}
+
+		for _, candidate := range candidates {
+			getBucketsRequest := &api.GetBucketsByCandidateRequest{
+				Name:   candidate.Name,
+				Height: strconv.Itoa(int(gravityHeight)),
+				Offset: uint32(0),
+				Limit:  math.MaxUint32,
+			}
+			getBucketsResponse, err := electionClient.GetBucketsByCandidate(ctx, getBucketsRequest)
+			if err != nil {
+				return errors.Wrapf(err, "failed to get buckets by candidate in epoch %d", epochNumber)
+			}
+			buckets := getBucketsResponse.Buckets
+			if err := p.updateVotingHistory(tx, buckets, candidate.Name, epochNumber); err != nil {
+				return errors.Wrapf(err, "failed to update voting history in epoch %d", epochNumber)
+			}
+			if err := p.updateVotingResult(tx, candidate, epochNumber); err != nil {
+				return errors.Wrapf(err, "failed to update voting result in epoch %d", epochNumber)
+			}
+		}
+	}
+	return nil
+}
+
+// GetVotingHistory gets voting history
+func (p *Protocol) GetVotingHistory(epochNumber uint64, candidateName string) ([]*VotingHistory, error) {
+	db := p.Store.GetDB()
+
+	getQuery := fmt.Sprintf("SELECT * FROM %s WHERE epoch_number=? AND candidate_name=?",
+		VotingHistoryTableName)
+	stmt, err := db.Prepare(getQuery)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to prepare get query")
+	}
+
+	rows, err := stmt.Query(epochNumber, candidateName)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to execute get query")
+	}
+
+	var votingHistory VotingHistory
+	parsedRows, err := s.ParseSQLRows(rows, &votingHistory)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse results")
+	}
+
+	if len(parsedRows) == 0 {
+		return nil, protocol.ErrNotExist
+	}
+
+	var votingHistoryList []*VotingHistory
+	for _, parsedRow := range parsedRows {
+		voting := parsedRow.(*VotingHistory)
+		votingHistoryList = append(votingHistoryList, voting)
+	}
+	return votingHistoryList, nil
+}
+
+// GetVotingResult gets voting result
+func (p *Protocol) GetVotingResult(epochNumber uint64, delegateName string) (*VotingResult, error) {
+	db := p.Store.GetDB()
+
+	getQuery := fmt.Sprintf("SELECT * FROM %s WHERE epoch_number=? AND delegate_name=?",
+		VotingResultTableName)
+	stmt, err := db.Prepare(getQuery)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to prepare get query")
+	}
+
+	rows, err := stmt.Query(epochNumber, delegateName)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to execute get query")
+	}
+
+	var votingResult VotingResult
+	parsedRows, err := s.ParseSQLRows(rows, &votingResult)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse results")
+	}
+
+	if len(parsedRows) == 0 {
+		return nil, protocol.ErrNotExist
+	}
+
+	if len(parsedRows) > 1 {
+		return nil, errors.New("only one row is expected")
+	}
+
+	return parsedRows[0].(*VotingResult), nil
+}
+
+func (p *Protocol) getCandidates(
+	chainClient iotexapi.APIServiceClient,
+	electionClient api.APIServiceClient,
+	height uint64,
+) ([]*api.Candidate, uint64, error) {
+	readStateRequest := &iotexapi.ReadStateRequest{
+		ProtocolID: []byte(poll.ProtocolID),
+		MethodName: []byte("GetGravityChainStartHeight"),
+		Arguments:  [][]byte{byteutil.Uint64ToBytes(height)},
+	}
+	readStateRes, err := chainClient.ReadState(context.Background(), readStateRequest)
+	if err != nil {
+		return nil, uint64(0), errors.Wrap(err, "failed to get gravity chain start height")
+	}
+	gravityChainStartHeight := byteutil.BytesToUint64(readStateRes.Data)
+
+	getCandidatesRequest := &api.GetCandidatesRequest{
+		Height: strconv.Itoa(int(gravityChainStartHeight)),
+		Offset: uint32(0),
+		Limit:  math.MaxUint32,
+	}
+
+	getCandidatesResponse, err := electionClient.GetCandidates(context.Background(), getCandidatesRequest)
+	if err != nil {
+		return nil, uint64(0), errors.Wrap(err, "failed to get candidates from election service")
+	}
+	return getCandidatesResponse.Candidates, gravityChainStartHeight, nil
+}
+
+func (p *Protocol) updateVotingHistory(tx *sql.Tx, buckets []*api.Bucket, candidateName string, epochNumber uint64) error {
+	for _, bucket := range buckets {
+		insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number,candidate_name,voter_address,votes,weighted_votes,"+
+			"remaining_duration) VALUES (?, ?, ?, ?, ?, ?)", VotingHistoryTableName)
+		if _, err := tx.Exec(insertQuery, epochNumber, candidateName, bucket.Voter, bucket.Votes, bucket.WeightedVotes, bucket.RemainingDuration); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *Protocol) updateVotingResult(tx *sql.Tx, candidate *api.Candidate, epochNumber uint64) error {
+	insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number,delegate_name,operator_address,reward_address,"+
+		"total_weighted_votes) VALUES (?, ?, ?, ?, ?)", VotingResultTableName)
+	if _, err := tx.Exec(insertQuery, epochNumber, candidate.Name, candidate.OperatorAddress, candidate.RewardAddress, candidate.TotalWeightedVotes); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ protocol/votings/protocol_test.go
@@ -0,0 +1,144 @@
+package votings
+
+import (
+	"context"
+	"database/sql"
+	"math"
+	"math/big"
+	"strconv"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/iotexproject/iotex-analytics/indexcontext"
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
+	"github.com/iotexproject/iotex-core/test/mock/mock_apiserviceclient"
+	"github.com/iotexproject/iotex-election/pb/api"
+	mock_election "github.com/iotexproject/iotex-election/test/mock/mock_apiserviceclient"
+	"github.com/iotexproject/iotex-proto/golang/iotexapi"
+	"github.com/stretchr/testify/require"
+
+	s "github.com/iotexproject/iotex-analytics/sql"
+	"github.com/iotexproject/iotex-analytics/testutil"
+)
+
+func TestProtocol(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	require := require.New(t)
+	ctx := context.Background()
+	testPath := "analytics.db"
+	testutil.CleanupPath(t, testPath)
+
+	store := s.NewSQLite3(testPath)
+	require.NoError(store.Start(ctx))
+	defer func() {
+		require.NoError(store.Stop(ctx))
+		testutil.CleanupPath(t, testPath)
+	}()
+
+	p := NewProtocol(store, uint64(24), uint64(15))
+
+	require.NoError(p.CreateTables(ctx))
+
+	blk, err := testutil.BuildCompleteBlock(uint64(1), uint64(361))
+	require.NoError(err)
+
+	chainClient := mock_apiserviceclient.NewMockServiceClient(ctrl)
+	electionClient := mock_election.NewMockAPIServiceClient(ctrl)
+	ctx = indexcontext.WithIndexCtx(context.Background(), indexcontext.IndexCtx{
+		ChainClient:    chainClient,
+		ElectionClient: electionClient,
+	})
+
+	chainClient.EXPECT().ReadState(gomock.Any(), gomock.Any()).Times(1).Return(&iotexapi.ReadStateResponse{
+		Data: byteutil.Uint64ToBytes(uint64(1000)),
+	}, nil)
+
+	electionClient.EXPECT().GetCandidates(gomock.Any(), gomock.Any()).Times(1).Return(
+		&api.CandidateResponse{
+			Candidates: []*api.Candidate{
+				{
+					Name:               "alfa",
+					OperatorAddress:    testutil.Addr1,
+					RewardAddress:      testutil.RewardAddr1,
+					TotalWeightedVotes: big.NewInt(1000).String(),
+				},
+				{
+					Name:               "bravo",
+					OperatorAddress:    testutil.Addr2,
+					RewardAddress:      testutil.RewardAddr2,
+					TotalWeightedVotes: big.NewInt(500).String(),
+				},
+			},
+		}, nil,
+	)
+
+	request1 := &api.GetBucketsByCandidateRequest{
+		Name:   "alfa",
+		Height: strconv.Itoa(1000),
+		Offset: uint32(0),
+		Limit:  math.MaxUint32,
+	}
+	electionClient.EXPECT().GetBucketsByCandidate(gomock.Any(), request1).Times(1).Return(
+		&api.BucketResponse{
+			Buckets: []*api.Bucket{
+				{
+					Voter:             "voter1",
+					Votes:             "300",
+					WeightedVotes:     "500",
+					RemainingDuration: "1day",
+				},
+				{
+					Voter:             "voter2",
+					Votes:             "400",
+					WeightedVotes:     "500",
+					RemainingDuration: "2days",
+				},
+			},
+		}, nil,
+	)
+
+	request2 := &api.GetBucketsByCandidateRequest{
+		Name:   "bravo",
+		Height: strconv.Itoa(1000),
+		Offset: uint32(0),
+		Limit:  math.MaxUint32,
+	}
+	electionClient.EXPECT().GetBucketsByCandidate(gomock.Any(), request2).Times(1).Return(
+		&api.BucketResponse{
+			Buckets: []*api.Bucket{
+				{
+					Voter:             "voter3",
+					Votes:             "200",
+					WeightedVotes:     "300",
+					RemainingDuration: "3days",
+				},
+				{
+					Voter:             "voter4",
+					Votes:             "150",
+					WeightedVotes:     "200",
+					RemainingDuration: "4days",
+				},
+			},
+		}, nil,
+	)
+
+	require.NoError(store.Transact(func(tx *sql.Tx) error {
+		return p.HandleBlock(ctx, tx, blk)
+	}))
+
+	votingHistoryList, err := p.GetVotingHistory(uint64(1), "alfa")
+	require.NoError(err)
+	require.Equal(2, len(votingHistoryList))
+	require.Equal("voter1", votingHistoryList[0].VoterAddress)
+	require.Equal("300", votingHistoryList[0].Votes)
+	require.Equal("500", votingHistoryList[0].WeightedVotes)
+	require.Equal("1day", votingHistoryList[0].RemainingDuration)
+
+	votingResult, err := p.GetVotingResult(uint64(1), "bravo")
+	require.NoError(err)
+	require.Equal(testutil.Addr2, votingResult.OperatorAddress)
+	require.Equal(testutil.RewardAddr2, votingResult.RewardAddress)
+	require.Equal("500", votingResult.TotalWeightedVotes)
+}
