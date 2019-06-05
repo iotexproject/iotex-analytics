b83176e6fa8ec75c9faebede4c1ae84e30db080c
Jiuqi Wang <wangjiuqi@wustl.edu>
2019-06-05 12:06:29 -0700

    time

--- graphql/resolver.go
+++ graphql/resolver.go
@@ -9,14 +9,19 @@ package graphql
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 
 	"github.com/iotexproject/iotex-analytics/queryprotocol/actions"
 	"github.com/iotexproject/iotex-analytics/queryprotocol/chainmeta"
 	"github.com/iotexproject/iotex-analytics/queryprotocol/productivity"
 	"github.com/iotexproject/iotex-analytics/queryprotocol/rewards"
 	"github.com/iotexproject/iotex-analytics/queryprotocol/votings"
+	"github.com/iotexproject/iotex-core/pkg/log"
+	"github.com/iotexproject/iotex-core/pkg/prometheustimer"
 ) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
 // Resolver is the resolver that handles graphql request
@@ -28,15 +33,54 @@ type Resolver struct {
 	CP *chainmeta.Protocol
 }
 
+var (
+	DBtimeFactory    *prometheustimer.TimerFactory
+	totaltimeFactory *prometheustimer.TimerFactory
+	flag             int
+)
+
+func init() {
+	go func() {
+		server := http.NewServeMux()
+		server.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":9001", server)
+	}()
+
+	timerFactoryDB, err := prometheustimer.New(
+		"DB_query_time_perf",
+		"",
+		[]string{"topic"},
+		[]string{"default"},
+	)
+	if err != nil {
+		log.L().Panic("Failed to generate prometheus timer factory.", zap.Error(err))
+	}
+	timerFactorytotal, err := prometheustimer.New(
+		"total_query_time_perf",
+		"",
+		[]string{"topic"},
+		[]string{"default"},
+	)
+	DBtimeFactory = timerFactoryDB
+	totaltimeFactory = timerFactorytotal
+	if err != nil {
+		log.L().Panic("Failed to generate prometheus timer factory.", zap.Error(err))
+	}
+}
+
 // Query returns a query resolver
 func (r *Resolver) Query() QueryResolver {
 	return &queryResolver{r}
 }
 
-type queryResolver struct{ *Resolver }
+type queryResolver struct {
+	*Resolver
+}
 
 // Rewards handles GetAccountReward request
 func (r *queryResolver) Rewards(ctx context.Context, startEpoch int, epochCount int, candidateName string) (*Reward, error) {
+	timerReward := totaltimeFactory.NewTimer("GetReward")
+	defer timerReward.End()
 	blockReward, epochReward, foundationBonus, err := r.RP.GetAccountReward(uint64(startEpoch), uint64(epochCount), candidateName)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get reward information")
@@ -50,7 +94,9 @@ func (r *queryResolver) Rewards(ctx context.Context, startEpoch int, epochCount 
 
 // Productivity handles GetProductivityHistory request
 func (r *queryResolver) Productivity(ctx context.Context, startEpoch int, epochCount int, producerName string) (*Productivity, error) {
+	timerPro := totaltimeFactory.NewTimer("GetProductivity")
 	production, expectedProduction, err := r.PP.GetProductivityHistory(uint64(startEpoch), uint64(epochCount), producerName)
+	defer timerPro.End()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get productivity information")
 	}
@@ -62,12 +108,17 @@ func (r *queryResolver) Productivity(ctx context.Context, startEpoch int, epochC
 
 // ActiveAccount handles GetActiveAccount request
 func (r *queryResolver) ActiveAccount(ctx context.Context, count int) ([]string, error) {
-	return r.AP.GetActiveAccount(count)
+	timerActive := totaltimeFactory.NewTimer("GetActiveAccount")
+	defer timerActive.End()
+	str, err := r.AP.GetActiveAccount(count)
+	return str, err
 }
 
 // VotingInformation handles GetVotingInformation request
 func (r *queryResolver) VotingInformation(ctx context.Context, epochNum int, delegateName string) (votingInfos []*VotingInfo, err error) {
+	timerVoting := totaltimeFactory.NewTimer("GetVotingInformation")
 	votingHistorys, err := r.VP.GetVotingInformation(epochNum, delegateName)
+	defer timerVoting.End()
 	if err != nil {
 		err = errors.Wrap(err, "failed to get voting information")
 		return
@@ -88,7 +139,9 @@ func (r *queryResolver) Bookkeeping(ctx context.Context, startEpoch int, epochCo
 		err = errors.New("percentage should be 0-100")
 		return
 	}
+	timerBook := totaltimeFactory.NewTimer("GetBookkeeping")
 	rets, err := r.RP.GetBookkeeping(uint64(startEpoch), uint64(epochCount), delegateName, percentage, includeFoundationBonus)
+	defer timerBook.End()
 	if err != nil {
 		err = errors.Wrap(err, "failed to get bookkeeping information")
 		return
@@ -109,6 +162,8 @@ func (r *queryResolver) AverageProductivity(ctx context.Context, startEpoch int,
 		err = errors.New("epoch num and count should be greater than 0")
 		return
 	}
+	timerAP := totaltimeFactory.NewTimer("GetAverageProductivity")
+	defer timerAP.End()
 	ap, err := r.PP.GetAverageProductivity(uint64(startEpoch), uint64(epochCount))
 	if err != nil {
 		return
@@ -124,6 +179,8 @@ func (r *queryResolver) ChainMeta(ctx context.Context, tpsBlockWindow int) (rets
 		err = errors.New("TPS block window should be greater than 0")
 		return
 	}
+	timerCM := totaltimeFactory.NewTimer("GetChainMeta")
+	defer timerCM.End()
 	ret, err := r.CP.GetChainMeta(tpsBlockWindow)
 	if err != nil {
 		err = errors.Wrap(err, "failed to get chain meta")
@@ -139,16 +196,22 @@ func (r *queryResolver) ChainMeta(ctx context.Context, tpsBlockWindow int) (rets
 
 // NumberOfActions handles NumberOfActions request
 func (r *queryResolver) NumberOfActions(ctx context.Context, startEpoch int, epochCount int) (string, error) {
+	timerNA := totaltimeFactory.NewTimer("GetNumberofActions")
+	defer timerNA.End()
 	return r.CP.GetNumberOfActions(uint64(startEpoch), uint64(epochCount))
 }
 
 // NumberOfWeightedVotes handles NumberOfWeightedVotes request
 func (r *queryResolver) NumberOfWeightedVotes(ctx context.Context, epochNumber int) (string, error) {
+	timerNW := totaltimeFactory.NewTimer("GetNumberOfWeightedVotes")
+	defer timerNW.End()
 	return r.VP.GetNumberOfWeightedVotes(uint64(epochNumber))
 }
 
 // NumberOfCandidates handles NumberOfCandidates request
 func (r *queryResolver) NumberOfCandidates(ctx context.Context, epochNumber int) (*NumberOfCandidates, error) {
+	timerNC := totaltimeFactory.NewTimer("GetNumberofCandidates")
+	defer timerNC.End()
 	numberOfCandidates, err := r.VP.GetNumberOfCandidates(uint64(epochNumber))
 	if err != nil {
 		return nil, err
