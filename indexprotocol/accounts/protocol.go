d4a793aab1e1d07d6278a79f774339168bdca6b1
lizhefeng <lizhefeng1993@gmail.com>
2019-09-05 17:28:43 -0700

    Modify accounts index protocol and redesign balance history table

--- indexprotocol/accounts/protocol.go
+++ indexprotocol/accounts/protocol.go
@@ -14,38 +14,29 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/iotexproject/iotex-analytics/indexprotocol"
+	"github.com/iotexproject/iotex-analytics/queryprotocol"
 	s "github.com/iotexproject/iotex-analytics/sql"
 )
 
 const (
 	// ProtocolID is the ID of protocol
 	ProtocolID = "accounts"
-	// AccountHistoryTableName is the table name of account history
-	AccountHistoryTableName = "account_history"
-	// AccountBalanceViewName is the view name of account balance
-	AccountBalanceViewName = "account_balance"
-	// EpochAddressIndexName is the index name of epoch number and address on account history table
-	EpochAddressIndexName = "epoch_address_index"
+	// BalanceHistoryTableName is the table name of balance history
+	BalanceHistoryTableName = "balance_history"
 )
 
 var specialActionHash = hash.ZeroHash256
 
 type (
-	// AccountHistory defines the base schema of "account history" table
-	AccountHistory struct {
+	// BalanceHistory defines the base schema of "balance history" table
+	BalanceHistory struct {
 		EpochNumber uint64
 		BlockHeight uint64
 		ActionHash  string
-		Address     string
-		In          string
-		Out         string
-	}
-
-	// AccountBalance defines the base schema of "account balance" view
-	AccountBalance struct {
-		EpochNumber   uint64
-		Address       string
-		BalanceChange string
+		ActionType  string
+		From        string
+		To          string
+		Amount      string
 	}
 )
 
@@ -70,23 +61,7 @@ func (p *Protocol) CreateTables(ctx context.Context) error {
 	// create block by action table
 	if _, err := p.Store.GetDB().Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
 		"(epoch_number DECIMAL(65, 0) NOT NULL, block_height DECIMAL(65, 0) NOT NULL, action_hash VARCHAR(64) NOT NULL, "+
-		"address VARCHAR(41) NOT NULL, `in` DECIMAL(65, 0) DEFAULT 0, `out` DECIMAL(65, 0) DEFAULT 0)", AccountHistoryTableName)); err != nil {
-		return err
-	}
-
-	var exist uint64
-	if err := p.Store.GetDB().QueryRow(fmt.Sprintf("SELECT COUNT(1) FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = "+
-		"DATABASE() AND TABLE_NAME = '%s' AND INDEX_NAME = '%s'", AccountHistoryTableName, EpochAddressIndexName)).Scan(&exist); err != nil {
-		return err
-	}
-	if exist == 0 {
-		if _, err := p.Store.GetDB().Exec(fmt.Sprintf("CREATE INDEX %s ON %s (epoch_number, address)", EpochAddressIndexName, AccountHistoryTableName)); err != nil {
-			return err
-		}
-	}
-
-	if _, err := p.Store.GetDB().Exec(fmt.Sprintf("CREATE OR REPLACE VIEW %s AS SELECT epoch_number, address, (SUM(`in`)-SUM(`out`)) AS balance_change "+
-		"FROM %s GROUP BY epoch_number, address", AccountBalanceViewName, AccountHistoryTableName)); err != nil {
+		"action_type TEXT NOT NULL, `from` VARCHAR(41) NOT NULL, `to` VARCHAR(41) NOT NULL, amount DECIMAL(65, 0) NOT NULL)", BalanceHistoryTableName)); err != nil {
 		return err
 	}
 
@@ -95,11 +70,21 @@ func (p *Protocol) CreateTables(ctx context.Context) error {
 
 // Initialize initializes actions protocol
 func (p *Protocol) Initialize(ctx context.Context, tx *sql.Tx, genesis *indexprotocol.Genesis) error {
+	db := p.Store.GetDB()
+	// Check existence
+	exist, err := queryprotocol.RowExists(db, fmt.Sprintf("SELECT * FROM %s WHERE action_hash = ?",
+		BalanceHistoryTableName), hex.EncodeToString(specialActionHash[:]))
+	if err != nil {
+		return errors.Wrap(err, "failed to check if the row exists")
+	}
+	if exist {
+		return nil
+	}
 	for addr, amount := range genesis.InitBalanceMap {
-		insertQuery := fmt.Sprintf("INSERT IGNORE INTO %s (epoch_number, block_height, action_hash, address, `in`) VALUES (?, ?, ?, ?, ?)",
-			AccountHistoryTableName)
-		if _, err := tx.Exec(insertQuery, uint64(0), uint64(0), hex.EncodeToString(specialActionHash[:]), addr, amount); err != nil {
-			return errors.Wrapf(err, "failed to update account history for address %s", addr)
+		insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number, block_height, action_hash, action_type, `from`, `to`, amount) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			BalanceHistoryTableName)
+		if _, err := tx.Exec(insertQuery, uint64(0), uint64(0), hex.EncodeToString(specialActionHash[:]), "genesis", "", addr, amount); err != nil {
+			return errors.Wrapf(err, "failed to update balance history for address %s", addr)
 		}
 	}
 	return nil
@@ -124,22 +109,19 @@ func (p *Protocol) HandleBlock(ctx context.Context, tx *sql.Tx, blk *block.Block
 		act := selp.Action()
 		switch act := act.(type) {
 		case *action.Transfer:
-			if act.Amount().Sign() > 0 {
-				if err := p.updateAccountHistory(tx, epochNumber, height, actionHash, dst, src, act.Amount().String()); err != nil {
-					return errors.Wrapf(err, "failed to update account history on height %d", height)
-				}
+			actionType := "transfer"
+			if err := p.updateBalanceHistory(tx, epochNumber, height, actionHash, actionType, dst, src, act.Amount().String()); err != nil {
+				return errors.Wrapf(err, "failed to update balance history on height %d", height)
 			}
 		case *action.DepositToRewardingFund:
-			if act.Amount().Sign() > 0 {
-				if err := p.updateAccountHistory(tx, epochNumber, height, actionHash, "", src, act.Amount().String()); err != nil {
-					return errors.Wrapf(err, "failed to update account history on height %d", height)
-				}
+			actionType := "depositToRewardingFund"
+			if err := p.updateBalanceHistory(tx, epochNumber, height, actionHash, actionType, "", src, act.Amount().String()); err != nil {
+				return errors.Wrapf(err, "failed to update balance history on height %d", height)
 			}
 		case *action.ClaimFromRewardingFund:
-			if act.Amount().Sign() > 0 {
-				if err := p.updateAccountHistory(tx, epochNumber, height, actionHash, src, "", act.Amount().String()); err != nil {
-					return errors.Wrapf(err, "failed to update account history on height %d", height)
-				}
+			actionType := "claimFromRewardingFund"
+			if err := p.updateBalanceHistory(tx, epochNumber, height, actionHash, actionType, src, "", act.Amount().String()); err != nil {
+				return errors.Wrapf(err, "failed to update balance history on height %d", height)
 			}
 		}
 	}
@@ -158,33 +140,32 @@ func (p *Protocol) HandleBlock(ctx context.Context, tx *sql.Tx, blk *block.Block
 		if gasFee.String() == "0" {
 			continue
 		}
-
-		if err := p.updateAccountHistory(tx, epochNumber, height, actHash, "", srcAddr, gasFee.String()); err != nil {
-			return errors.Wrapf(err, "failed to update account history with address %s", srcAddr)
+		if err := p.updateBalanceHistory(tx, epochNumber, height, actHash, "gasFee", "", srcAddr, gasFee.String()); err != nil {
+			return errors.Wrapf(err, "failed to update balance history with address %s", srcAddr)
 		}
 	}
 
 	return nil
 }
 
-func (p *Protocol) getAccountHistory(address string) ([]*AccountHistory, error) {
+func (p *Protocol) getBalanceHistory(address string) ([]*BalanceHistory, error) {
 	db := p.Store.GetDB()
 
-	getQuery := fmt.Sprintf("SELECT * FROM %s WHERE address=?",
-		AccountHistoryTableName)
+	getQuery := fmt.Sprintf("SELECT * FROM %s WHERE `from`=? OR `to`=?",
+		BalanceHistoryTableName)
 	stmt, err := db.Prepare(getQuery)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to prepare get query")
 	}
 	defer stmt.Close()
 
-	rows, err := stmt.Query(address)
+	rows, err := stmt.Query(address, address)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to execute get query")
 	}
 
-	var accountHistory AccountHistory
-	parsedRows, err := s.ParseSQLRows(rows, &accountHistory)
+	var balanceHistory BalanceHistory
+	parsedRows, err := s.ParseSQLRows(rows, &balanceHistory)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to parse results")
 	}
@@ -193,69 +174,28 @@ func (p *Protocol) getAccountHistory(address string) ([]*AccountHistory, error) 
 		return nil, indexprotocol.ErrNotExist
 	}
 
-	var accountHistoryList []*AccountHistory
+	var balanceHistoryList []*BalanceHistory
 	for _, parsedRow := range parsedRows {
-		acctChange := parsedRow.(*AccountHistory)
-		accountHistoryList = append(accountHistoryList, acctChange)
+		balChange := parsedRow.(*BalanceHistory)
+		balanceHistoryList = append(balanceHistoryList, balChange)
 	}
-	return accountHistoryList, nil
+	return balanceHistoryList, nil
 }
 
-func (p *Protocol) getAccountBalanceChange(epochNumber uint64, address string) (*AccountBalance, error) {
-	db := p.Store.GetDB()
-
-	getQuery := fmt.Sprintf("SELECT * FROM %s WHERE epoch_number=? AND address=?",
-		AccountBalanceViewName)
-	stmt, err := db.Prepare(getQuery)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to prepare get query")
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(epochNumber, address)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to execute get query")
-	}
-
-	var accountBalance AccountBalance
-	parsedRows, err := s.ParseSQLRows(rows, &accountBalance)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse results")
-	}
-
-	if len(parsedRows) == 0 {
-		return nil, indexprotocol.ErrNotExist
-	}
-
-	if len(parsedRows) > 1 {
-		return nil, errors.New("only one row is expected")
-	}
-
-	return parsedRows[0].(*AccountBalance), nil
-}
-
-func (p *Protocol) updateAccountHistory(
+func (p *Protocol) updateBalanceHistory(
 	tx *sql.Tx,
 	epochNumber uint64,
 	blockHeight uint64,
 	actionHash hash.Hash256,
-	inAddr string,
-	outAddr string,
+	actionType string,
+	to string,
+	from string,
 	amount string,
 ) error {
-	if inAddr != "" {
-		insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number, block_height, action_hash, address, `in`) VALUES (?, ?, ?, ?, ?)",
-			AccountHistoryTableName)
-		if _, err := tx.Exec(insertQuery, epochNumber, blockHeight, hex.EncodeToString(actionHash[:]), inAddr, amount); err != nil {
-			return errors.Wrapf(err, "failed to update account history for address %s", inAddr)
-		}
-	}
-	if outAddr != "" {
-		insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number, block_height, action_hash, address, `out`) VALUES (?, ?, ?, ?, ?)",
-			AccountHistoryTableName)
-		if _, err := tx.Exec(insertQuery, epochNumber, blockHeight, hex.EncodeToString(actionHash[:]), outAddr, amount); err != nil {
-			return errors.Wrapf(err, "failed to update account history for address %s", outAddr)
-		}
+	insertQuery := fmt.Sprintf("INSERT INTO %s (epoch_number, block_height, action_hash, action_type, `from`, `to`, amount) VALUES (?, ?, ?, ?, ?, ?, ?)",
+		BalanceHistoryTableName)
+	if _, err := tx.Exec(insertQuery, epochNumber, blockHeight, hex.EncodeToString(actionHash[:]), actionType, from, to, amount); err != nil {
+		return errors.Wrap(err, "failed to update balance history")
 	}
 	return nil
 }
