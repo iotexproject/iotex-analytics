cbf8261d8c22b5229fd445c8f3bc643509c41bd8
newyear6565 <newyear6565@163.com>
2020-07-29 16:47:56 +0800

    fix sort by timestamp issue on byType query (#276)

--- queryprotocol/actions/protocol.go
+++ queryprotocol/actions/protocol.go
@@ -28,7 +28,7 @@ const (
 	selectActionHistoryByTimestamp = "SELECT action_hash, block_hash, timestamp, action_type, `from`, `to`, amount, t1.gas_price*t1.gas_consumed " +
 		"FROM %s AS t1 LEFT JOIN %s AS t2 ON t1.block_height=t2.block_height " +
 		"WHERE timestamp >= ? AND timestamp <= ? ORDER BY `timestamp` desc limit ?,?"
-	selectActionHistoryByType = "SELECT action_hash, block_hash, `timestamp`, action_type, `from`, `to`, amount, gas_price*gas_consumed FROM %s AS t1 LEFT JOIN (SELECT block_height,block_hash,`timestamp` FROM %s ORDER BY `timestamp` DESC) t2 ON t1.block_height=t2.block_height WHERE action_type =? limit ?,?"
+	selectActionHistoryByType = "SELECT action_hash, block_hash, `timestamp`, action_type, `from`, `to`, amount, gas_price*gas_consumed FROM %s AS t1 LEFT JOIN (SELECT block_height,block_hash,`timestamp` FROM %s) t2 ON t1.block_height=t2.block_height WHERE action_type =? ORDER BY `timestamp` DESC limit ?,?"
 	selectActionHistoryByHash = "SELECT action_hash, block_hash, timestamp, action_type, `from`, `to`, amount, t1.gas_price*t1.gas_consumed FROM %s " +
 		"AS t1 LEFT JOIN %s AS t2 ON t1.block_height=t2.block_height WHERE action_hash = ?"
 	selectActionHistoryByAddress = "SELECT action_hash, block_hash, timestamp, action_type, `from`, `to`, amount, t1.gas_price*t1.gas_consumed FROM %s " +
