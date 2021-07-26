fe48c7b0d23ce8a8e388880733064658f40b1523
CoderZhi <thecoderzhi@gmail.com>
2021-07-26 12:41:58 -0700

    Revert "set sql connections"

--- sql/storebase.go
+++ sql/storebase.go
@@ -11,7 +11,6 @@ import (
 	"database/sql"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/rs/zerolog"
 
@@ -71,10 +70,6 @@ func (s *storeBase) Start(ctx context.Context) error {
 		return err
 	}
 	s.db = db
-	s.db.SetMaxOpenConns(400)
-	s.db.SetMaxIdleConns(10)
-	s.db.SetConnMaxLifetime(5 * time.Minute)
-
 	return nil
 }
 
